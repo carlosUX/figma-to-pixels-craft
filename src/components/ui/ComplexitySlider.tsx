f9739f2ece42cb61c9fb45708ca97d634477fa1d
Carlos <carlos.marin@gmail.com>
2025-05-05 09:18:25 -0700

    bot

--- src/components/ui/ComplexitySlider.tsx
+++ src/components/ui/ComplexitySlider.tsx
@@ -155,7 +155,7 @@ export const ComplexitySlider: React.FC = () => {
       </div>
 
       {/* Description text - reduced margin between slider and description */}
-      <div className="text-sm text-[#201F1F] mt-[0px]">
+      <div className="text-sm text-[#201F1F] mb-[0px]">
         {getLevelDescription()}
       </div>
     </div>
